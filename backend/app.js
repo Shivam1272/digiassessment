b5717283d4cdd964acd349e982b5bbe4fdfa37bf
Shivam <sspatel120702@gmail.com>
2023-11-09 12:32:14 +0530

    replace a mongoose method

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.put("/api/students/:id", async (req, res) => {
     const { name, location, cgpa } = req.body;
     const studentId = req.params.id;
     // console.log(name, location, cgpa);
-    const updatedStudent = await Student.findByIdAndUpdate(studentId, {
+    const updatedStudent = await Student.findOneAndUpdate(studentId, {
       name,
       location,
       cgpa,
